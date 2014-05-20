25a7545080b742e81a33bf29ffabebfded1ec836
Taha Hachana <tahahachana@gmail.com>
2014-05-20 14:42:32 +0100

    Update skin module

--- Sitelet/Skin.fs
+++ Sitelet/Skin.fs
@@ -1,58 +1,53 @@
-﻿namespace Website
+﻿module Sitelet.Skin
 
 open System.Web
 open IntelliFactory.WebSharper.Sitelets
 open IntelliFactory.WebSharper.Sitelets.Content
 
-module Skin =
+type DefaultPage =
+    {
+        Title : string
+        MetaDescription : string
+        Body : HtmlElement
+        Footer : HtmlElement
+    }
 
-    type DefaultPage =
+let loadFrequency =
+#if DEBUG
+    Content.Template.PerRequest
+#else
+    Content.Template.Once
+#endif
+
+let makeTemplate<'T> path =
+    let path' = HttpContext.Current.Server.MapPath path
+    Template<'T>(path', loadFrequency)
+    
+let makeDefaultTemplate path =
+    makeTemplate<DefaultPage> path
+    |> fun template ->
+        template.With("title", fun page -> page.Title)
+            .With("meta-description", fun page -> page.MetaDescription)
+            .With("body", fun page -> page.Body)
+            .With("footer", fun page -> page.Footer)
+
+let withTemplate<'T> template title metaDescription makeBody footer : Content<'T> =
+    WithTemplate template
+    <| fun context ->
         {
-            Title           : string
-            MetaDescription : string
-            Body            : HtmlElement
-            Footer          : HtmlElement
+            Title = title
+            MetaDescription = metaDescription
+            Body = makeBody context
+            Footer = footer
         }
 
-        static member Make title metaDescription makeBody context footer =
-            {
-                Title           = title
-                MetaDescription = metaDescription
-                Body            = makeBody context
-                Footer          = footer
-            }
-
-    let loadFrequency =
-        #if DEBUG
-            Content.Template.PerRequest
-        #else
-            Content.Template.Once
-        #endif
-
-    let makeTemplate<'T> path =
-        let path' = HttpContext.Current.Server.MapPath path
-        Template<'T>(path', loadFrequency)
-    
-    let makeDefaultTemplate path =
-        makeTemplate<DefaultPage> path
-        |> fun x ->
-            x.With("title"          , fun x -> x.Title)
-             .With("meta-description", fun x -> x.MetaDescription)
-             .With("body"           , fun x -> x.Body)
-             .With("footer"         , fun x -> x.Footer)
-
-    let withTemplate<'T> template title metaDescription makeBody footer : Content<'T> =
-        WithTemplate template
-        <| fun context ->
-            DefaultPage.Make title metaDescription makeBody context footer
-
-module ExtJsSkin =
-
-    type Page = { Body : Content.HtmlElement }
-
-    let template =
-        let path = HttpContext.Current.Server.MapPath("~/HTML/ExtJS.html")
-        Content.Template<Page>(path).With("body", fun x -> x.Body)
-
-    let withTemplate body : Content<'T> =
-        Content.WithTemplate template <| fun context -> { Body = body }
+//module ExtJsSkin =
+//
+//    type Page = { Body : Content.HtmlElement }
+//
+//    let template =
+//        let path = HttpContext.Current.Server.MapPath("~/HTML/ExtJS.html")
+//        Content.Template<Page>(path).With("body", fun page -> page.Body)
+//
+//    let withTemplate body : Content<'T> =
+//        Content.WithTemplate template <| fun context -> { Body = body }

6f8f0494734131eb686929cb67db3d4fbb003cdd
Taha Hachana <tahahachana@gmail.com>
2014-05-21 08:54:11 +0100

    Update controller

--- Sitelet/Controller.fs
+++ Sitelet/Controller.fs
@@ -1,31 +1,29 @@
-﻿module Sitelet.
+﻿module Sitelet.Controller
 
-module Controller =
-    open IntelliFactory.WebSharper.Sitelets
-    open Model
+open IntelliFactory.WebSharper.Sitelets
+open Model
 
-    let private protect view =
-        match UserSession.GetLoggedInUser() with
-            | None -> Content.Redirect <| Login None
-            | _    -> view
+let private protect view =
+    match UserSession.GetLoggedInUser() with
+        | None -> Content.Redirect <| Login None
+        | _    -> view
 
-    let private logout() =
-        UserSession.Logout ()
-        Content.Redirect Home
+let private logout() =
+    UserSession.Logout ()
+    Content.Redirect Home
     
-    let controller =
-        let handle = function
-            | About          -> Views.about
-            | Admin          -> protect Views.admin
-            | Error          -> Views.error
-            | Home           -> Views.home
-            | Login action   -> Views.login action
-            | Logout         -> logout()
-            | Snippet (id, path) -> Views.snippet id path
-//            | Highlight      -> Views.highlight
-            | Tagged tag     -> Views.tagged tag
+let controller =
+    let handle = function
+        | About          -> Views.about
+        | Admin          -> protect Views.admin
+        | Error          -> Views.error
+        | Home           -> Views.home
+        | Login action   -> Views.login action
+        | Logout         -> logout()
+        | Snippet (id, path) -> Views.snippet id path
+        | Tagged tag     -> Views.tagged tag
 //            | Extjs pageId   -> Views.extjs pageId
-            | Search (q, id) -> Views.search q id
-            | Rss            -> Views.rss
-            | _              -> Content.ServerError
-        { Handle = handle }
+        | Search (q, id) -> Views.search q id
+        | Rss            -> Views.rss
+        | _              -> Content.ServerError
+    { Handle = handle }
